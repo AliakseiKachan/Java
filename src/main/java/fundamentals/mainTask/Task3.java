c884fa284e4fd98b67dd606304646d438fd63066
Aliaksei Kachan <untitled89@inbox.ru>
2020-10-19 16:08:18 +0300

    modify task 3 of main task (create method for checking input values)

--- src/main/java/fundamentals/mainTask/Task3.java
+++ src/main/java/fundamentals/mainTask/Task3.java
@@ -12,46 +12,35 @@ public class Task3 {
 
         Scanner input = new Scanner(System.in);
 
-        int startPosition; // Начальное значение диапазона
-        int endPosition; // Конечное значение диапазона
-        int countOfNumbers; // Количество  генерируемых случайных чисел
+        Task3 task3 = new Task3();
+
+        int startPosition = 0; // Начальное значение диапазона
+        int endPosition = 0; // Конечное значение диапазона
+        int countOfNumbers = 0; // Количество  генерируемых случайных чисел
         String answer;
 
         do {
-
             System.out.println("Input start position of range:");
-            while (!input.hasNextInt()) {
-                System.out.println("Input correct number, not string or char!");
-                input.next();
-            }
-
+            task3.checkInputValues(input, startPosition);
             startPosition = input.nextInt();
 
             System.out.println("Input end position of range:");
-            while (!input.hasNextInt()) {
-                System.out.println("Input correct number, not string or char!");
-                input.next();
-            }
-
+            task3.checkInputValues(input, endPosition);
             endPosition = input.nextInt();
 
             System.out.println("Input count of numbers:");
-            while (!input.hasNextInt()) {
-                System.out.println("Input correct number, not string or char!");
-                input.next();
-            }
-
+            task3.checkInputValues(input, countOfNumbers);
             countOfNumbers = input.nextInt();
 
             for (int i = 0; i < countOfNumbers; i++) {
                 int randomNumber = startPosition + (int) (Math.random() * ((endPosition - startPosition) + 1));
 
-// -500 + (int) (Math.random() * ((500-(-500)) + 1))
-// -500 + (int) (Math.random() * 1001)
-// По умолчанию генерируются вещественные значения [0, 1), 1 исключительно
-// 1001 * 0 = 0, 1001 * 1 = 1001, получаем [0, 1001), 1001 исключительно
-// В результате приведения к типу int получаем [0, 1000], 1000 включительно
-// -500 + 0 = -500, -500 + 1000 = 500, получаем [-500, 500]
+//          -500 + (int) (Math.random() * ((500-(-500)) + 1))
+//          -500 + (int) (Math.random() * 1001)
+//          По умолчанию генерируются вещественные значения [0, 1), 1 исключительно
+//          1001 * 0 = 0, 1001 * 1 = 1001, получаем [0, 1001), 1001 исключительно
+//          В результате приведения к типу int получаем [0, 1000], 1000 включительно
+//          -500 + 0 = -500, -500 + 1000 = 500, получаем [-500, 500]
 
                 System.out.print(" " + randomNumber); // Без перехода на новую строку
             }
@@ -68,5 +57,24 @@ public class Task3 {
             answer = input.next();
 
         } while ("y".equals(answer));
+    }
+
+    private void checkInputValues (Scanner input, int checkValue) {
+
+        int count = 4;
+
+        while (!input.hasNextInt() && count != 0) {
+
+            count--;
+
+            if (count == 0) {
+                System.out.println("You have no any attempts, GOODBYE!");
+                System.exit(0);
+            }
+
+            System.out.println("You have " + count + " attempts to input correct number, not string or char!");
+            input.next();
+        }
+
     }
 }
